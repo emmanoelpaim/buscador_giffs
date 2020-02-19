9eac7ae69611b897cfc53254c1b3ab28461ff9d8
EMMANOEL <emmanoel@asav.org.br>
2020-02-19 13:20:42 -0300

    first commit

---
+++ android/app/src/main/kotlin/br/com/emmanoel/buscador/buscador_giffs/MainActivity.kt
@@ -0,0 +1,12 @@
+package br.com.emmanoel.buscador.buscador_giffs
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
